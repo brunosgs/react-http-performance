fc4254c7db25ec674e3705d852bdcee8f9aee29f
brunosgs <brunosoges@gmail.com>
2025-07-21 22:00:09 -0300

    requisição http
    feat: executando uma requisição http para a api do json server e usando a informação para popular a tabela de transações.

--- http-performance/src/App.tsx
+++ http-performance/src/App.tsx
@@ -1,13 +1,22 @@
-import { ThemeProvider } from "styled-components";
+import isPropValid from '@emotion/is-prop-valid';
+import {
+   StyleSheetManager,
+   ThemeProvider
+} from "styled-components";
 import { Transactions } from "./pages/transactions/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
    return (
-      <ThemeProvider theme={defaultTheme}>
-         <Transactions />
-         <GlobalStyle />
-      </ThemeProvider>
+      <StyleSheetManager enableVendorPrefixes
+         shouldForwardProp={(propName, elementToBeRendered) => {
+            return typeof elementToBeRendered === 'string' ? isPropValid(propName) : true;
+         }}>
+         <ThemeProvider theme={defaultTheme}>
+            <Transactions />
+            <GlobalStyle />
+         </ThemeProvider>
+      </StyleSheetManager>
    )
 }
