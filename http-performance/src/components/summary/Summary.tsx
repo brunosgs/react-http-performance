6e0dde8f2907df0ac3dbd1688c575c79b189ac02
brunosgs <brunosoges@gmail.com>
2025-07-30 21:32:39 -0300

    resumo de transações
    build: atualizado dependências;
    feat: adicionado calculo do resumo para mostrar as entradas, saídas e total das contas;
    feat: adicionado formatador para números referente a dinheiro e data.

--- http-performance/src/components/summary/Summary.tsx
+++ http-performance/src/components/summary/Summary.tsx
@@ -5,6 +5,7 @@ import {
 } from '@phosphor-icons/react';
 import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormatter } from '../../utils/formatter';
 import {
    SummaryCard,
    SummaryContainer
@@ -12,7 +13,22 @@ import {
 
 export function Summary() {
    const { transactions } = useContext(TransactionsContext)
-   console.log(transactions)
+   const summary = transactions.reduce((acc, transaction) => {
+      if (transaction.type === 'income') {
+         acc.income += transaction.price
+         acc.total += transaction.price
+      } else {
+         acc.outcome += transaction.price
+         acc.total -= transaction.price
+      }
+
+      return acc
+   },
+      {
+         income: 0,
+         outcome: 0,
+         total: 0
+      })
 
    return (
       <SummaryContainer>
@@ -21,21 +37,21 @@ export function Summary() {
                <span>Entradas</span>
                <ArrowCircleUpIcon size={32} color="#00b37e" />
             </header>
-            <strong>R$ 17.400,00</strong>
+            <strong>{priceFormatter.format(summary.income)}</strong>
          </SummaryCard>
          <SummaryCard>
             <header>
                <span>Saídas</span>
                <ArrowCircleDownIcon size={32} color="#f75a68" />
             </header>
-            <strong>R$ 17.400,00</strong>
+            <strong>{priceFormatter.format(summary.outcome)}</strong>
          </SummaryCard>
          <SummaryCard variantColor='green'>
             <header>
                <span>Total</span>
                <CurrencyDollarIcon size={32} color="#fff" />
             </header>
-            <strong>R$ 17.400,00</strong>
+            <strong>{priceFormatter.format(summary.total)}</strong>
          </SummaryCard>
       </SummaryContainer>
    )
