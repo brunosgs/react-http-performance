6b7a981c4b3ec4e9bcaf619a0c78a723ceacbbdb
brunosgs <brunosoges@gmail.com>
2025-08-13 11:28:08 -0300

    nova transação
    feat: criando uma nova transação para que, ao finalizar, seja atualizado o contexto e assim atualizado a tabela de transações.

--- http-performance/src/contexts/TransactionsContext.tsx
+++ http-performance/src/contexts/TransactionsContext.tsx
@@ -15,9 +15,17 @@ interface Transaction {
    createdAt: string
 }
 
+interface CreateTransactionInput {
+   description: string
+   type: 'income' | 'outcome'
+   category: string
+   price: number
+}
+
 interface TransactionContextType {
    transactions: Transaction[],
-   fetchTransactions: (query?: string) => Promise<void>
+   fetchTransactions: (query?: string) => Promise<void>,
+   createTransaction: (data: CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -33,6 +41,8 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
    async function fetchTransactions(query?: string) {
       const response = await api.get('transactions', {
          params: {
+            _sort: 'createdAt',
+            _order: 'desc',
             q: query
          }
       })
@@ -40,12 +50,35 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       setTransactions(response.data)
    }
 
+   async function createTransaction(data: CreateTransactionInput) {
+      const {
+         description,
+         price,
+         category,
+         type
+      } = data
+
+      const response = await api.post('transactions', {
+         description,
+         type,
+         category,
+         price,
+         createdAt: new Date()
+      })
+
+      setTransactions(state => [...state, response.data])
+   }
+
    useEffect(() => {
       fetchTransactions()
    }, [])
 
    return (
-      <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+      <TransactionsContext.Provider value={{
+         transactions,
+         fetchTransactions,
+         createTransaction
+      }}>
          {children}
       </TransactionsContext.Provider>
    )
