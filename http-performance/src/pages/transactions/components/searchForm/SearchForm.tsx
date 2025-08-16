32b5d9ff0d3d480e56b78a8d91d136c8a86cf3dd
brunosgs <brunosoges@gmail.com>
2025-08-16 15:45:31 -0300

    memo
    doc: explicação de quando utilizar o memo() da biblioteca react.

--- http-performance/src/pages/transactions/components/searchForm/SearchForm.tsx
+++ http-performance/src/pages/transactions/components/searchForm/SearchForm.tsx
@@ -6,6 +6,30 @@ import * as z from 'zod';
 import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { SearchFormContainer } from "./styles";
 
+/**
+ * Por que um componente renderiza?
+ * 
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedades);
+ * - Parent rerendered (component pai renderizou);
+ * 
+ * Qual o fluxo de renderização?
+ * 
+ * 1. O react recria o HTML da interface do componente;
+ * 2. Compara a versão do HTML recriada com a versão anterior;
+ * 3. Se mudou alguma coisa, ele reescreve o HTML na tela.
+ * 
+ * Memo:
+ * 0. Alterou os Hooks ou Props (Hooks changed ou Props changed) fazendo 
+ * um deep comparison?
+ * 0.1. Comparar a versão anterior dos hooks e props;
+ * 0.2. Se mudou algo, permiti o novo fluxo de renderização. Se não,
+ * não vai executar o fluxo de renderização.
+ * 
+ * OBS: Usar o memo, só quando o componente tiver uma complexidade na estrutura
+ * de dados complexas. 
+ */
+
 const searchFromSchema = z.object({
    query: z.string()
 })
